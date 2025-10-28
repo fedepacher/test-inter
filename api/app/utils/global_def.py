4d2a53d584db2e68f072266c25f3c720b2afb8a9
fedepacher <fede_pacher@hotmail.com>
2025-10-28 00:08:43 -0300

    feat: added global definitions

---
+++ api/app/utils/global_def.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class AcceptedLanguages(str, Enum):
+    """ doc """
+    ES = "es"
+    ES_ES = "es-ES"
+    ES_MX = "es-MX"
+    ES_AR = "es-AR"
+    ES_CO = "es-CO"
+    EN = "en"
+    EN_US = "en-US"
+    EN_GB = "en-GB"
+    EN_AU = "en-AU"
+    EN_CA = "en-CA"
+    PT = "pt"
+    PT_BR = "pt-BR"
+    PT_PT = "pt-PT"
+    DEFAULT = "es"
+
+
+class StatusEnum(str, Enum):
+    """Enumeration for general statuses."""
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+    DELETED = "deleted"
