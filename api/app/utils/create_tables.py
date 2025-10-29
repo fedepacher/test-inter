23ce48602adc2838c3a4e7823c40b3793e6d9cca
fedepacher <fede_pacher@hotmail.com>
2025-10-29 14:56:39 -0300

    fix: removed unused tables

--- api/app/utils/create_tables.py
+++ api/app/utils/create_tables.py
@@ -6,10 +6,8 @@ from api.app.model.athlete_model import Athletes
 from api.app.model.country_model import Countries
 from api.app.model.gender_model import Genders
 from api.app.model.institution_model import Institutions
-from api.app.model.position_model import Positions
 from api.app.model.professional_role_model import Professional_Roles
 from api.app.model.profile_model import Profiles
-from api.app.model.sport_model import Sports
 from api.app.model.user_model import Users
 from api.app.utils.db import db
 
@@ -23,9 +21,7 @@ def create_tables():
             Countries,
             Genders,
             Institutions,
-            Positions,
             Professional_Roles,
             Profiles,
-            Sports,
             Users,
         ], safe=True)  # `safe=True` will not raise an exception if tables already exist
